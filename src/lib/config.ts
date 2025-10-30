1f0c8885d68a2238720725ee7fd74c28ca35f052
Aditya Bagayatkar <111139561+AdityaB09@users.noreply.github.com>
2025-10-29 20:18:13 -0500

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,8 @@
 export const API_BASE =
   process.env.NEXT_PUBLIC_API_BASE?.replace(/\/+$/, "") || "http://localhost:8080";
+
+export const API_BASE =
+  process.env.BACKEND_URL?.replace(/\/+$/, "") || "http://localhost:8080";
+
+export const API_BASE =
+  process.env.NEXT_PUBLIC_BACKEND_URL?.replace(/\/+$/, "") || "http://localhost:8080";
