40838798a08835fcfab6b441de212c0b8cd3c9a4
Aditya Bagayatkar <111139561+AdityaB09@users.noreply.github.com>
2025-10-29 22:51:37 -0500

    Update page.tsx

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -50,9 +50,9 @@ export default function SearchPage() {
           {hits.map((h, i) => (
             <ReviewCard key={i} text={h.text} score={h.score} />
           ))}
-        {/* </div>
-        <SimilarPanel items={hits} />
-      </div> */}
+        </div>
+        {/* <SimilarPanel items={hits} /> */}
+      </div>
     </div>
   );
 }
